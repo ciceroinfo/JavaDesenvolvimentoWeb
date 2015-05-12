0dd53f714d72b3eadbfc24dbb697bdb8e43f7000
cicero <cicero@cicero-computer>
2015-05-12 00:56:24 -0300

    dao

--- CiceroServlet/src/com/ciceroinfo/servlet/FirstServlet.java
+++ CiceroServlet/src/com/ciceroinfo/servlet/FirstServlet.java
@@ -2,10 +2,14 @@ package com.ciceroinfo.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ciceroinfo.bo.Contato;
+import com.ciceroinfo.dao.ContatoDAO;
 
 public class FirstServlet extends HttpServlet {
 
@@ -14,6 +18,12 @@ public class FirstServlet extends HttpServlet {
 	public void service(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
 
+		try {
+			new ContatoDAO().add(new Contato());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 		PrintWriter out = response.getWriter();
 		
 		out.println("<html><body><h1>Test Servlet</h1></body></html>");
