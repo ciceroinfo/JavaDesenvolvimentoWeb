53e3a596c24d039868a59d8ac6434dfc864c1877
cicero <cicero@cicero-computer>
2015-05-12 23:57:54 -0300

    teste

--- CiceroServlet/src/com/ciceroinfo/servlet/AdicionaContatoServlet.java
+++ CiceroServlet/src/com/ciceroinfo/servlet/AdicionaContatoServlet.java
@@ -1,13 +1,15 @@
 package com.ciceroinfo.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,8 +22,21 @@ public class AdicionaContatoServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		log("Iniciando a servlet");
+	}
+
+	@Override
+	public void destroy() {
+		super.destroy();
+		log("Destruindo a servlet");
+	}
+
+	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
+			throws IOException, ServletException {
 
 		try {
 			DAO<Contato> dao = new ContatoDAO();
@@ -48,8 +63,11 @@ public class AdicionaContatoServlet extends HttpServlet {
 		} catch (SQLException | ParseException e) {
 			e.printStackTrace();
 		}
-		PrintWriter out = response.getWriter();
 
-		out.println("<html><body><h1>Test Servlet</h1></body></html>");
+		RequestDispatcher rd = request
+				.getRequestDispatcher("adiciona-contato.html");
+
+		rd.forward(request, response);
+
 	}
 }
